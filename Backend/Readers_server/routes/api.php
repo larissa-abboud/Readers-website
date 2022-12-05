b1a5ff2ef794862372f8a06e449b4ee9b345d03c
larissa-abboud <lariabboud@gmail.com>
2022-12-05 18:38:27 +0200

    backend: creating version routes

--- Backend/Readers_server/routes/api.php
+++ Backend/Readers_server/routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::group(["prefix" => "v0.1"], function(){
+    
+});
