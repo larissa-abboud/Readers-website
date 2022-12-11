6cfd0f2403ef9d5ca54b1436bfe6f79f00f6fa68
larissa-abboud <lariabboud@gmail.com>
2022-12-11 23:30:58 +0200

    Frontend: creating landing page

--- Frontend/js/scripts.js
+++ Frontend/js/scripts.js
@@ -1,8 +1,8 @@
-const workshop_pages = {};
+const readers_pages = {};
 
-const base_url = "http://localhost/mobile/";
+const base_url = "http://localhost/";
 
-workshop_pages.Console = (title, values, oneValue = true) => {
+readers_pages.Console = (title, values, oneValue = true) => {
     console.log('---' + title + '---');
     if(oneValue){
         console.log(values);
@@ -14,19 +14,19 @@ workshop_pages.Console = (title, values, oneValue = true) => {
     console.log('--/' + title + '---');
 }
 
-workshop_pages.loadFor = (page) => {
-    eval("workshop_pages.load_" + page + "();");
+readers_pages.loadFor = (page) => {
+    eval("readers_pages.load_" + page + "();");
 }
 
-workshop_pages.getAPI = async (api_url) => {
+readers_pages.getAPI = async (api_url) => {
     try{
         return await axios(api_url);
     }catch(error){
-        workshop_pages.Console("Error from Linking (GET)", error);
+        readers_pages.Console("Error from Linking (GET)", error);
     }
 }
 
-workshop_pages.postAPI = async (api_url, api_data, api_token = null) => {
+readers_pages.postAPI = async (api_url, api_data, api_token = null) => {
         try{
             return await axios.post(
                 api_url,
@@ -38,15 +38,15 @@ workshop_pages.postAPI = async (api_url, api_data, api_token = null) => {
                 }
             );
         }catch(error){
-            workshop_pages.Console("Error from Linking (POST)", error);
+            readers_pages.Console("Error from Linking (POST)", error);
         }
 }
 
-workshop_pages.load_landing = async () => {
+readers_pages.load_landing = async () => {
    
 
 }
 
-workshop_pages.load_profile = () => {
+readers_pages.load_profile = () => {
     alert("HI FROM PROFILE PAGE");
 }
