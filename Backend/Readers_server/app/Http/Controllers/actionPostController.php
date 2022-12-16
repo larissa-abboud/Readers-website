ce5bccb66284293e00fb6d89b9f241a453a6591a
larissa-abboud <lariabboud@gmail.com>
2022-12-16 18:02:36 +0200

    Backend: editing action post controller methods

--- Backend/Readers_server/app/Http/Controllers/actionPostController.php
+++ Backend/Readers_server/app/Http/Controllers/actionPostController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class actionPostController extends Controller
 {
-    //
+    //update comment count
+    //update like count
+    function updateLikes($id){
+
+        $post = Post::find($id);
+        $post->likes = $post->likes +1;
+        
+        $post->save();
+
+        return response()->json([
+            "result" => true 
+        ]);
+    }
+    function updateComments($id){
+
+        $post = Post::find($id);
+        $post->comments = $post->comments +1;
+        
+        $post->save();
+
+        return response()->json([
+            "result" => true 
+        ]);
+    }
+
 }
